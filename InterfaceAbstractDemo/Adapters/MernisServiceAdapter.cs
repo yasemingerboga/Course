428696cf83fe52183e406caa2f9bae5879783b66
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-02-08 18:37:29 +0300

    interface and abstract demo in real life project

---
+++ InterfaceAbstractDemo/Adapters/MernisServiceAdapter.cs
@@ -0,0 +1,19 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using MernisServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceAbstractDemo.Adapters
+{
+    public class MernisServiceAdapter : ICustomerCheckService
+    {
+        public bool CheckIfRealPerson(Customer customer)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+            TCKimlikNoDogrulaResponse response = client.TCKimlikNoDogrulaAsync(Convert.ToInt64(customer.NationalityId), customer.FirstName, customer.SecondName, customer.DateOfBirth.Year).Result;
+            return response.Body.TCKimlikNoDogrulaResult;
+        }
+    }
+}
---
+++ InterfaceAbstractDemo/Concrete/StarbucksCustomerManager.cs
@@ -0,0 +1,28 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceAbstractDemo.Concrete
+{
+    public class StarbucksCustomerManager : BaseCustomerManager
+    {
+        private ICustomerCheckService customerCheckService;
+        public StarbucksCustomerManager(ICustomerCheckService customerCheckService)
+        {
+            this.customerCheckService = customerCheckService;
+        }
+        public override void Save(Customer customer)
+        {
+            if (customerCheckService.CheckIfRealPerson(customer))
+            {
+                base.Save(customer);
+            }
+            else
+            {
+                throw new Exception("Not a valid person!");
+            }
+        }
+    }
+}
