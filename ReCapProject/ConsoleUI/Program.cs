bb0a5b9d53ffe2ba098c3160914c63e7c0690739
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-02-09 18:46:41 +0300

    .gitignore file, .gitattributes and ReCap project added.

---
+++ ReCapProject/ConsoleUI/Program.cs
@@ -0,0 +1,52 @@
+﻿using DataAccess.Abstract;
+using DataAccess.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>
+            {
+                new Car{ ID=1, BrandID=1, ColorID=1, DailyPrice=45, Description="Megane", ModelYear=2007},
+                new Car{ ID=2, BrandID=1, ColorID=2, DailyPrice=55, Description="Megane", ModelYear=2020},
+                new Car{ ID=3, BrandID=2, ColorID=2, DailyPrice=50, Description="Mini Cooper", ModelYear=2010},
+                new Car{ ID=4, BrandID=3, ColorID=3, DailyPrice=90, Description="Porsche ", ModelYear=2020},
+                new Car{ ID=5, BrandID=3, ColorID=4, DailyPrice=80, Description="Porsche ", ModelYear=2015},
+            };
+            ICarDal carDal = new InMemoryCarDal(cars);
+            Console.WriteLine("Before: ");
+            foreach (var c in carDal.GetAll())
+            {
+                Console.WriteLine(c.ID + " " + c.Description + " Daily Price is: " + c.DailyPrice + "$");
+            }
+            Car car = new Car { ID = 6, BrandID = 3, ColorID = 1, DailyPrice = 120, Description = "Porsche", ModelYear = 2021 };
+            carDal.Add(car);
+            Console.WriteLine("After adding process: ");
+            foreach (var c in carDal.GetAll())
+            {
+                Console.WriteLine(c.ID + " " + c.Description + " Daily Price is: " + c.DailyPrice + "$");
+            }
+            Car car1 = new Car { ID = 6, BrandID = 3, ColorID = 1, DailyPrice = 100, Description = "Porsche", ModelYear = 2021 };
+            carDal.Update(car1);
+            Console.WriteLine("After updating process: ");
+            foreach (var c in carDal.GetAll())
+            {
+                Console.WriteLine(c.ID + " " + c.Description + " Daily Price is: " + c.DailyPrice + "$");
+            }
+            carDal.Delete(car1);
+            Console.WriteLine("Car with ID number 6 is deleted. After deletion process: ");
+            foreach (var c in carDal.GetAll())
+            {
+                Console.WriteLine(c.ID + " " + c.Description + " Daily Price is: " + c.DailyPrice + "$");
+            }
+            Console.WriteLine("-----------------------------------------------");
+            Car car2 = carDal.GetByID(3);
+            Console.WriteLine("(input ID = 3) Car with ID number : " + car2.ID + " " + car2.Description);
+        }
+    }
+}
