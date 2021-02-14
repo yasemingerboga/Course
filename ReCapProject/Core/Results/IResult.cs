8c7ae94bf7ba5cf2cfa86fb1d0178966c66028dc
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-02-14 16:24:41 +0300

    Result class for method returning void added.

---
+++ ReCapProject/Core/Results/IResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Results
+{
+    //for methods returning void
+    public interface IResult
+    {
+        //readonly (constant)
+        bool Success { get; }
+        string Message { get; }
+    }
+}
