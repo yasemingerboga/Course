a454f3aad1b93a648d124302505d2d2efb472b97
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-02-14 16:34:35 +0300

    Messages class added

---
+++ ReCapProject/Business/Constants/Messages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string CarAdded = "Car added.";
+        public static string CarDeleted = "Car deleted.";
+        public static string CarUpdated = "Car updated.";
+    }
+}
