951d5d25485e0289f0ce802947d314dc3f9022e6
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-02-15 16:10:46 +0300

    User,Rental and Customer entities created and CRUD operations implemented.

--- ReCapProject/Business/Constants/Messages.cs
+++ ReCapProject/Business/Constants/Messages.cs
@@ -7,8 +7,26 @@ namespace Business.Constants
     public static class Messages
     {
         public static string CarAdded = "Car added.";
+        public static string CustomerAdded = "Customer added.";
+        public static string UserAdded = "User added.";
+        public static string RentalAdded = "Rental added.";
+
         public static string CarDeleted = "Car deleted.";
+        public static string CustomerDeleted = "Customer deleted.";
+        public static string UserDeleted = "User deleted.";
+        public static string RentalDeleted = "Rental deleted.";
+
         public static string CarUpdated = "Car updated.";
+        public static string CustomerUpdated = "Customer updated.";
+        public static string UserUpdated = "User updated.";
+        public static string RentalUpdated = "Rental updated.";
+
         public static string CarListed = "Cars listed.";
+        public static string CustomerListed = "Customers listed.";
+        public static string UserListed = "User listed.";
+        public static string RentalListed = "Rental listed.";
+
+        public static string RentalDetails = "Rental details listed.";
+        public static string NotAvailable = "Car is not available.";
     }
 }
---
+++ ReCapProject/DataAccess/Concrete/EntityFramework/EfRentalDal.cs
@@ -0,0 +1,85 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTO;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfRentalDal : IRentalDal
+    {
+        public void Add(Rental entity)
+        {
+            using (ReCapProjectDatabaseContext context = new ReCapProjectDatabaseContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Rental entity)
+        {
+            using (ReCapProjectDatabaseContext context = new ReCapProjectDatabaseContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public Rental Get(Expression<Func<Rental, bool>> filter)
+        {
+            using (ReCapProjectDatabaseContext context = new ReCapProjectDatabaseContext())
+            {
+                return context.Set<Rental>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<Rental> GetAll(Expression<Func<Rental, bool>> filter = null)
+        {
+            using (ReCapProjectDatabaseContext context = new ReCapProjectDatabaseContext())
+            {
+                return filter == null
+                     ? context.Set<Rental>().ToList()
+                     : context.Set<Rental>().Where(filter).ToList();
+            }
+        }
+
+        public List<RentalDetailDto> GetRentalDetails(Expression<Func<Rental, bool>> filter = null)
+        {
+            using (ReCapProjectDatabaseContext context = new ReCapProjectDatabaseContext())
+            {
+                var result = from r in filter is null ? context.Rentals : context.Rentals.Where(filter)
+                             join c in context.Cars on r.CarId equals c.CarId
+                             join cu in context.Customers on r.CustomerId equals cu.Id
+                             join u in context.Users on cu.UserId equals u.Id
+                             select new RentalDetailDto
+                             {
+                                 RentId = r.Id,
+                                 CarName = c.Name,
+                                 CustomerName = u.FirstName,
+                                 CustomerSurname = u.LastName,
+                                 CompanyName = cu.CompanyName,
+                                 RentDate = r.RentDate,
+                                 ReturnDate = (DateTime)r.ReturnDate
+                             };
+                return result.ToList();
+            }
+        }
+
+        public void Update(Rental entity)
+        {
+            using (ReCapProjectDatabaseContext context = new ReCapProjectDatabaseContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
