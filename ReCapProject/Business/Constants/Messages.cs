68c1244d06abf1143e793c5c5a522b0257de4624
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-02-16 19:26:59 +0300

    Messages updated.

--- ReCapProject/Business/Constants/Messages.cs
+++ ReCapProject/Business/Constants/Messages.cs
@@ -10,23 +10,33 @@ namespace Business.Constants
         public static string CustomerAdded = "Customer added.";
         public static string UserAdded = "User added.";
         public static string RentalAdded = "Rental added.";
+        public static string BrandAdded = "Brand added.";
 
         public static string CarDeleted = "Car deleted.";
         public static string CustomerDeleted = "Customer deleted.";
         public static string UserDeleted = "User deleted.";
         public static string RentalDeleted = "Rental deleted.";
+        public static string BrandDeleted = "Brand deleted.";
 
         public static string CarUpdated = "Car updated.";
         public static string CustomerUpdated = "Customer updated.";
         public static string UserUpdated = "User updated.";
         public static string RentalUpdated = "Rental updated.";
+        public static string BrandUpdated = "Brand updated.";
 
         public static string CarListed = "Cars listed.";
         public static string CustomerListed = "Customers listed.";
-        public static string UserListed = "User listed.";
-        public static string RentalListed = "Rental listed.";
+        public static string UserListed = "Users listed.";
+        public static string RentalListed = "Rentals listed.";
+        public static string BrandListed = "Brands listed.";
 
         public static string RentalDetails = "Rental details listed.";
         public static string NotAvailable = "Car is not available.";
+
+        public static string CarGetById = "Car Brought in with id number.";
+        public static string CustomerGetById = "Customer Brought in with id number.";
+        public static string UserGetById = "User Brought in with id number.";
+        public static string RentalGetById = "Rental Brought in with id number.";
+        public static string BrandGetById = "Brand Brought in with id number.";
     }
 }
