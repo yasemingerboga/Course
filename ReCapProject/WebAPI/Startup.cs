7000ea2206b4eb28ec668b0f2effa581b130e7df
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-02-17 13:19:26 +0300

    Rentals WebAPI added.

--- ReCapProject/WebAPI/Startup.cs
+++ ReCapProject/WebAPI/Startup.cs
@@ -38,6 +38,8 @@ namespace WebAPI
             services.AddSingleton<IColorDal, EfColorDal>();
             services.AddSingleton<ICustomerService, CustomerManager>();
             services.AddSingleton<ICustomerDal, EfCustomerDal>();
+            services.AddSingleton<IRentalService, RentalManager>();
+            services.AddSingleton<IRentalDal, EfRentalDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
