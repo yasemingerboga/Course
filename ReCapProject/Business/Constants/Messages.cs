51912ab2e525b71b99363f2929582089ad7ca007
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-02-17 12:37:12 +0300

    Messages updated.

--- ReCapProject/Business/Constants/Messages.cs
+++ ReCapProject/Business/Constants/Messages.cs
@@ -11,24 +11,28 @@ namespace Business.Constants
         public static string UserAdded = "User added.";
         public static string RentalAdded = "Rental added.";
         public static string BrandAdded = "Brand added.";
+        public static string ColorAdded = "Color added.";
 
         public static string CarDeleted = "Car deleted.";
         public static string CustomerDeleted = "Customer deleted.";
         public static string UserDeleted = "User deleted.";
         public static string RentalDeleted = "Rental deleted.";
         public static string BrandDeleted = "Brand deleted.";
+        public static string ColorDeleted = "Color deleted.";
 
         public static string CarUpdated = "Car updated.";
         public static string CustomerUpdated = "Customer updated.";
         public static string UserUpdated = "User updated.";
         public static string RentalUpdated = "Rental updated.";
         public static string BrandUpdated = "Brand updated.";
+        public static string ColorUpdated = "Color updated.";
 
         public static string CarListed = "Cars listed.";
         public static string CustomerListed = "Customers listed.";
         public static string UserListed = "Users listed.";
         public static string RentalListed = "Rentals listed.";
         public static string BrandListed = "Brands listed.";
+        public static string ColorListed = "Colors listed.";
 
         public static string RentalDetails = "Rental details listed.";
         public static string NotAvailable = "Car is not available.";
@@ -38,6 +42,7 @@ namespace Business.Constants
         public static string UserGetById = "User brought in with id number.";
         public static string RentalGetById = "Rental brought in with id number.";
         public static string BrandGetById = "Brand brought in with id number.";
+        public static string ColorGetById = "Color brought in with id number.";
 
         public static string GetCarDetails = "Car details brought.";
         public static string CarGetColorId = "Car details brought by color id.";
