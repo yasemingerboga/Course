1d639bb8a4ec8f8446f8989b370c67988c941357
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-03-03 13:02:55 +0300

    add ValidationRules

---
+++ ReCapProject/Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.DependencyResolvers.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.Name).MinimumLength(2);
+            RuleFor(c => c.DailyPrice).GreaterThan(0);
+            RuleFor(c => c.DailyPrice).NotEmpty();
+            RuleFor(c => c.ModelYear).Must(IsValidYear).WithMessage("Year is invalid.");
+        }
+
+        private bool IsValidYear(int arg)
+        {
+            return (arg.ToString().StartsWith("20") && arg.ToString().Length == 4);
+        }
+    }
+}
