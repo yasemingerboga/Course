f8ea07d754535ed0d4bcf8783b7fb8e3518794df
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-03-03 13:43:43 +0300

    add validator class

---
+++ ReCapProject/Business/ValidationRules/FluentValidation/RentalValidator.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class RentalValidator : AbstractValidator<Rental>
+    {
+        public RentalValidator()
+        {
+            RuleFor(r => r.ReturnDate).NotNull();
+            RuleFor(r => r.CarId).NotNull();
+            RuleFor(r => r.CustomerId).NotNull();
+        }
+    }
+}
