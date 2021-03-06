2df041523a3e679f736f11bda7eec781c08323cb
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-03-06 20:01:05 +0300

    bugfix Entity Framework

--- ReCapProject/DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ ReCapProject/DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -13,44 +13,6 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EfCarDal : EfEntityRepositoryBase<Car, ReCapProjectDatabaseContext>, ICarDal
     {
-        public void Add(Car entity)
-        {
-            using (ReCapProjectDatabaseContext context = new ReCapProjectDatabaseContext())
-            {
-                var addedEntity = context.Entry(entity);
-                addedEntity.State = EntityState.Added;
-                context.SaveChanges();
-            }
-        }
-
-        public void Delete(Car entity)
-        {
-            using (ReCapProjectDatabaseContext context = new ReCapProjectDatabaseContext())
-            {
-                var deletedEntity = context.Entry(entity);
-                deletedEntity.State = EntityState.Deleted;
-                context.SaveChanges();
-            }
-        }
-
-        public Car Get(Expression<Func<Car, bool>> filter)
-        {
-            using (ReCapProjectDatabaseContext context = new ReCapProjectDatabaseContext())
-            {
-                return context.Set<Car>().SingleOrDefault(filter);
-            }
-        }
-
-        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
-        {
-            using (ReCapProjectDatabaseContext context = new ReCapProjectDatabaseContext())
-            {
-                return filter == null
-                     ? context.Set<Car>().ToList()
-                     : context.Set<Car>().Where(filter).ToList();
-            }
-        }
-
         public List<CarDetailDto> GetCarDetails()
         {
             using (ReCapProjectDatabaseContext context = new ReCapProjectDatabaseContext())
@@ -69,16 +31,6 @@ namespace DataAccess.Concrete.EntityFramework
                                  DailyPrice = ca.DailyPrice,
                              };
                 return result.ToList();
-            }
-        }
-
-        public void Update(Car entity)
-        {
-            using (ReCapProjectDatabaseContext context = new ReCapProjectDatabaseContext())
-            {
-                var updatedEntity = context.Entry(entity);
-                updatedEntity.State = EntityState.Modified;
-                context.SaveChanges();
             }
         }
     }
--- ReCapProject/DataAccess/Concrete/EntityFramework/EfCarImageDal.cs
+++ ReCapProject/DataAccess/Concrete/EntityFramework/EfCarImageDal.cs
@@ -10,7 +10,7 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfCustomerDal : EfEntityRepositoryBase<Customer, ReCapProjectDatabaseContext>, ICustomerDal
+    public class EfCarImageDal : EfEntityRepositoryBase<CarImage, ReCapProjectDatabaseContext>, ICarImageDal
     {
     }
 }
