32310fb8c2a7739cf17c157de4092d5825e173a1
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-03-11 16:22:34 +0300

    Add EfUserDal

--- ReCapProject/DataAccess/Concrete/EntityFramework/EfUserDal.cs
+++ ReCapProject/DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -1,4 +1,5 @@
 ﻿using Core.DataAccess.EntityFramework;
+using Core.Entities.Concrete;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
@@ -10,7 +11,19 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfUserDal : EfEntityRepositoryBase<User, ReCapProjectDatabaseContext>,IUserDal
+    public class EfUserDal : EfEntityRepositoryBase<User, ReCapProjectDatabaseContext>, IUserDal
     {
+        public List<OperationClaim> GetClaims(User user)
+        {
+            using (var context=new ReCapProjectDatabaseContext())
+            {
+                var resut = from operationClaim in context.OperationClaims
+                            join userOperationClaim in context.UserOperationClaims
+                            on operationClaim.Id equals userOperationClaim.OperationClaimId
+                            where operationClaim.Id == user.Id
+                            select new OperationClaim { Id = operationClaim.Id, Name = operationClaim.Name };
+                return resut.ToList();
+            }
+        }
     }
 }
