54c69c89550d89aa2f2458cc1426382fe76a94bd
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-03-11 16:19:22 +0300

    update validators

--- ReCapProject/Business/ValidationRules/FluentValidation/CarValidator.cs
+++ ReCapProject/Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -10,6 +10,11 @@ namespace Business.DependencyResolvers.ValidationRules.FluentValidation
     {
         public CarValidator()
         {
+            RuleFor(c => c.BrandId).NotEmpty();
+            RuleFor(c => c.ColorId).NotEmpty();
+            RuleFor(c => c.Name).NotEmpty();
+            RuleFor(c => c.ModelYear).NotEmpty();
+            RuleFor(c => c.Descriptions).NotEmpty();
             RuleFor(c => c.Name).MinimumLength(2);
             RuleFor(c => c.DailyPrice).GreaterThan(0);
             RuleFor(c => c.DailyPrice).NotEmpty();
