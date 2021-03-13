da404e8cea00ae0d683cff6c15b96348e6c81a0a
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-03-13 20:25:43 +0300

    bugfix

--- ReCapProject/DataAccess/Concrete/EntityFramework/EfUserDal.cs
+++ ReCapProject/DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -16,14 +16,14 @@ namespace DataAccess.Concrete.EntityFramework
     {
         public List<OperationClaim> GetClaims(User user)
         {
-            using (var context=new ReCapProjectDatabaseContext())
+            using (var context = new ReCapProjectDatabaseContext())
             {
-                var resut = from operationClaim in context.OperationClaims
-                            join userOperationClaim in context.UserOperationClaims
-                            on operationClaim.Id equals userOperationClaim.OperationClaimId
-                            where operationClaim.Id == user.Id
-                            select new OperationClaim { Id = operationClaim.Id, Name = operationClaim.Name };
-                return resut.ToList();
+                var result = from operationClaim in context.OperationClaims
+                             join userOperationClaim in context.UserOperationClaims
+                                 on operationClaim.Id equals userOperationClaim.OperationClaimId
+                             where userOperationClaim.UserId == user.Id
+                             select new OperationClaim { Id = operationClaim.Id, Name = operationClaim.Name };
+                return result.ToList();
             }
         }
     }
