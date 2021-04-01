8ff403c3de4a72e62ad3c55b498eabff0866f29d
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-04-01 22:21:44 +0300

    add CardsController

---
+++ ReCapProject/WebAPI/Controllers/CardsController.cs
@@ -0,0 +1,62 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardsController : ControllerBase
+    {
+        ICardService _cardService;
+        public CardsController(ICardService cardService)
+        {
+            _cardService = cardService;
+        }
+        [HttpPost("add")]
+        public IActionResult Add(Card card)
+        {
+            var result = _cardService.Add(card);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpPost("delete")]
+        public IActionResult Delete(Card card)
+        {
+            var result = _cardService.Delete(card);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpPost("update")]
+        public IActionResult Update(Card card)
+        {
+            var result = _cardService.Update(card);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpGet("check")]
+        public IActionResult Check(string cardNumber)
+        {
+            var result = _cardService.Check(cardNumber);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
