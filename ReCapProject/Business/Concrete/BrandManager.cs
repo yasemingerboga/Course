037a4ca8cadf2e04772fc2cce65839b8b285e0e4
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-04-04 22:38:47 +0300

    update

--- ReCapProject/Business/Concrete/BrandManager.cs
+++ ReCapProject/Business/Concrete/BrandManager.cs
@@ -27,21 +27,26 @@ namespace Business.Concrete
 
         public IDataResult<List<Brand>> GetAll()
         {
-            return new SuccessDataResult<List<Brand>>( _brandDal.GetAll(),Messages.BrandListed);
+            return new SuccessDataResult<List<Brand>>(_brandDal.GetAll(), Messages.BrandListed);
         }
 
         public IDataResult<Brand> GetById(int brandId)
         {
-            return new SuccessDataResult<Brand>(_brandDal.Get(b => b.BrandId == brandId),Messages.BrandGetById);
+            return new SuccessDataResult<Brand>(_brandDal.Get(b => b.BrandId == brandId), Messages.BrandGetById);
         }
 
         [ValidationAspect(typeof(BrandValidator))]
         public IResult Add(Brand brand)
         {
+            if (_brandDal.GetAll(b => b.Name.ToLower() == brand.Name.ToLower()).Count > 0)
+            {
+                return new ErrorResult(Messages.BrandCannotAdded);
+            }
             _brandDal.Add(brand);
             return new SuccessResult(Messages.BrandAdded);
         }
 
+        [ValidationAspect(typeof(BrandValidator))]
         public IResult Update(Brand brand)
         {
             _brandDal.Update(brand);
