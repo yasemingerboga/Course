c6465c77b053226e4061e80b923baecaa165a9f4
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-04-11 21:44:12 +0300

    add Credit card validation

---
+++ ReCapProject/Business/ValidationRules/FluentValidation/CreditCardValidator.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CreditCardValidator : AbstractValidator<CreditCard>
+    {
+        public CreditCardValidator()
+        {
+            RuleFor(c => c.CreditCardNumber).NotEmpty();
+            RuleFor(c => c.Cvc).NotEmpty();
+            RuleFor(c => c.ExpirationMonth).NotEmpty();
+            RuleFor(c => c.ExpirationYear).NotEmpty();
+            RuleFor(c => c.NameOnTheCard).NotEmpty();
+            RuleFor(c => c.ExpirationMonth).LessThanOrEqualTo("12");
+            RuleFor(c => c.ExpirationMonth).GreaterThan("0");
+            RuleFor(c => c.ExpirationYear).GreaterThanOrEqualTo(DateTime.Now.Year.ToString());
+        }
+    }
+}
