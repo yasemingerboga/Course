4606b23e271aecb09d632cbf23aedeffeaead696
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-04-11 21:43:16 +0300

    add IUserInformationService

---
+++ ReCapProject/Business/Abstract/IUserInformationService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Results;
+using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IUserInformationService
+    {
+        IResult Add(UserInformation userInformation);
+        IResult Delete(UserInformation userInformation);
+        IResult Update(UserInformation userInformation);
+        IDataResult<UserInformation> GetByUserId(int userId);
+    }
+}
