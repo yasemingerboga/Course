5f659e6b43c71a810362e6a371004fd9b3b2436b
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-04-11 21:51:12 +0300

    add user information sql codes

---
+++ CarRentalProject_SQL/dbo.UserInformations.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[UserInformations] (
+    [Id]               INT           IDENTITY (1, 1) NOT NULL,
+    [UserId]           INT           NOT NULL,
+    [UserCreditCardId] INT           NOT NULL,
+    [FirstName]        VARCHAR (50)  NOT NULL,
+    [LastName]         VARCHAR (50)  NOT NULL,
+    [Email]            VARCHAR (50)  NOT NULL,
+    [FindeksPoint]     VARCHAR (50)  DEFAULT ((0)) NULL,
+    [ImagePath]        VARCHAR (255) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id]),
+    FOREIGN KEY ([UserCreditCardId]) REFERENCES [dbo].[CreditCards] ([Id])
+);
+
