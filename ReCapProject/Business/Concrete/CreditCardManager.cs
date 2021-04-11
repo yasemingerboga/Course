529e6dee5aa9f6af70dafcd672673651cd892ed5
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-04-11 21:43:39 +0300

    add CreditCardManager

---
+++ ReCapProject/Business/Concrete/CreditCardManager.cs
@@ -0,0 +1,51 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
+using Core.Aspects.Autofac.Validation;
+using Core.Results;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CreditCardManager : ICreditCardService
+    {
+        ICreditCardDal _creditCardDal;
+        public CreditCardManager(ICreditCardDal creditCardDal)
+        {
+            _creditCardDal = creditCardDal;
+        }
+
+        [ValidationAspect(typeof(CreditCardValidator))]
+        public IResult Add(CreditCard creditCard)
+        {
+            _creditCardDal.Add(creditCard);
+            return new SuccessResult(Messages.CreditCardAdded);
+        }
+
+        [ValidationAspect(typeof(CreditCardValidator))]
+        public IResult Delete(CreditCard creditCard)
+        {
+            _creditCardDal.Delete(creditCard);
+            return new SuccessResult(Messages.CreditCardDeleted);
+        }
+
+        [CacheAspect]
+        public IDataResult<List<CreditCard>> GetByUserId(int userId)
+        {
+            return new SuccessDataResult<List<CreditCard>>(_creditCardDal.GetAll(c => c.UserId == userId),Messages.GetCreditCardByUserId);
+        }
+
+        [ValidationAspect(typeof(CreditCardValidator))]
+        public IResult Update(CreditCard creditCard)
+        {
+            _creditCardDal.Update(creditCard);
+            return new SuccessResult(Messages.CreditCardUpdated);
+        }
+    }
+}
