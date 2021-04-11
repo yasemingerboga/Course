f4afa7552197c98b729589f9600f0a36bdbd81c7
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-04-11 21:46:25 +0300

    add credit card class

---
+++ ReCapProject/Entities/Concrete/CreditCard.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class CreditCard : IEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string NameOnTheCard { get; set; }
+        public string CreditCardNumber { get; set; }
+        public string Cvc { get; set; }
+        public string ExpirationMonth { get; set; }
+        public string ExpirationYear { get; set; }
+    }
+}
