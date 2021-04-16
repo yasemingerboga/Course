dc9cca00d891106d7663a0aca27ed4f2cb0f6865
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-04-16 03:51:43 +0300

    update

--- ReCapProject/Core/Utilities/Security/JWT/AccessToken.cs
+++ ReCapProject/Core/Utilities/Security/JWT/AccessToken.cs
@@ -8,5 +8,6 @@ namespace Core.Utilities.Security.JWT
     {
         public string Token { get; set; }
         public DateTime Expiration { get; set; }
+        public int userId { get; set; }
     }
 }
--- ReCapProject/Core/Utilities/Security/JWT/JwtHelper.cs
+++ ReCapProject/Core/Utilities/Security/JWT/JwtHelper.cs
@@ -34,7 +34,8 @@ namespace Core.Utilities.Security.JWT
             return new AccessToken
             {
                 Token = token,
-                Expiration = _accessTokenExpiration
+                Expiration = _accessTokenExpiration,
+                userId = user.Id
             };
 
         }
